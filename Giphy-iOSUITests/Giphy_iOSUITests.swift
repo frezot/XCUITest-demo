1ecb72fb26ae4502dc11203658f77336e8d4307f
Fedor <frezot@gmail.com>
2020-04-25 00:28:04 +0700

    add target and scheme for UITests

---
+++ Giphy-iOSUITests/Giphy_iOSUITests.swift
@@ -0,0 +1,20 @@
+import XCTest
+
+class Giphy_iOSUITests: XCTestCase {
+
+    override func setUp() {
+        continueAfterFailure = false
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    override func tearDown() {
+
+    }
+
+    func testExample() {
+        //TODO: implement
+        sleep(1)
+    }
+
+}
